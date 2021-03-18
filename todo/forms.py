6b3b48b6cf40017dc5a53affee50bc3904f2dc7a
adam181189 <adam181189@gmail.com>
2021-03-18 16:26:07 +0000

    sadfa

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Item
 
+
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        field = ["name", "done"]
+        fields = ["name", "done"]
